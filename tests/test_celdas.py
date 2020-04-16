53fee622a4210b172cd311736a88d2dca95a3b7c
Maite <maite@example.com>
2020-04-16 19:28:24 -0300

    Agrego test al menos una celda ocup por colum

--- tests/test_celdas.py
+++ tests/test_celdas.py
@@ -23,3 +23,14 @@ def test_mayor_quince():
         for celda in fila:
             contador = contador + celda
     assert contador < 16
+
+def test_al_menos_una_ocupada():
+    contador = 0
+    contador2 = 9
+    mi_carton = carton()
+    
+    for i in range(contador2):
+        if (mi_carton[0][i] + mi_carton[1][i] + mi_carton[2][i]) >= 1:
+            contador += 1
+
+    assert contador == contador2
