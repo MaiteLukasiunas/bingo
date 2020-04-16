980c2d89e0d8a8a4dfecb4ec89f8c2c5b55092ef
Maite <maite@example.com>
2020-04-16 11:10:23 -0300

    No mas No menos de quince

--- tests/test_celdas.py
+++ tests/test_celdas.py
@@ -7,3 +7,19 @@ def test_contar_celdas_ocupadas():
         for celda in fila:
             contador = contador + celda
     assert contador == 15
+
+def test_menor_quince():
+    mi_carton = carton()
+    contador = 0
+    for fila in mi_carton:
+        for celda in fila:
+            contador = contador + celda
+    assert contador > 14
+
+def test_mayor_quince():
+    mi_carton = carton()
+    contador = 0
+    for fila in mi_carton:
+        for celda in fila:
+            contador = contador + celda
+    assert contador < 16
